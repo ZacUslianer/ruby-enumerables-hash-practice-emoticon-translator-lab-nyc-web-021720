4f3a132754163e46f0288b6d7be978a0ed13ccd8
Zac Uslianer <zacuslianer@gmail.com>
2020-02-11 23:58:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,6 +2,7 @@ require 'yaml'
 require 'pry'
 
 emoticons = YAML.load_file('lib/emoticons.yml')
+<<<<<<< HEAD
 
 def load_library(emoticons)
   get_meaning = {}
@@ -30,4 +31,21 @@ def get_english_meaning(emoticons, emoji)
   return "Sorry, that emoticon was not found"
   end
   translation
+=======
+
+def load_library(emoticons)
+  get_meaning = {}
+  get_emoticon = {}
+  emoticons = YAML.load_file(emoticons)
+  emoticons.each do |english, emo|
+    get_meaning[emo[1]] = english
+    get_emoticon[emo[0]] = emo[1]
+  end
+  {:get_meaning => get_meaning, :get_emoticon => get_emoticon}
+end
+
+def get_japanese_emoticon(emoticons, emoji)
+  library = load_library(emoticons)
+binding.pry
+>>>>>>> 5c8bdc15e0120fb6d15d363afcaaec9c8855748c
 end
